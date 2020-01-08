c0d11d712c8351db2bd373ec7f09c57373df78ff
IgorNovozhilov <maildrakon@gmail.com>
2020-01-08 09:32:53 +0300

    test build

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ import { execSync } from 'child_process'
 const env = {
   NODE_ENV: 'test'
 }
+const encoding = 'utf8'
 
-execSync('npx babel lib-sample -d cjs/lib-sample', { env })
-execSync('npx babel test -d cjs/test', { env })
+execSync('npx babel lib-sample -d cjs/lib-sample', { env, encoding })
+execSync('npx babel test -d cjs/test', { env, encoding })
