f4184949b05386c9df7d8c921a9545efe3dfe670
ia.novozhilov <ia.novozhilov@tensor.ru>
2020-01-15 13:12:36 +0300

    upd deps

--- lib-sample/example.js
+++ lib-sample/example.js
@@ -1,8 +1,16 @@
+/**
+ * Тестовый класс
+ *
+ * @returns {Test}
+ */
 export class Test {
+
   #test1 = 'test: '
   #test2 = () => 'ok'
 
+  /** @returns {string} */
   getResult() {
     return this.#test1 + this.#test2()
   }
+
 }
